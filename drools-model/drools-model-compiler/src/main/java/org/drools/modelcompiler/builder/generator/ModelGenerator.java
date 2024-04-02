6bff6ac9d2a3f96537106c17ebcdc03d819e615a
jalbarran <jaal1388@gmail.com>
2024-04-02 15:33:01 -0400

    ruleAttribute processing order kept

--- drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/ModelGenerator.java
+++ drools-model/drools-model-compiler/src/main/java/org/drools/modelcompiler/builder/generator/ModelGenerator.java
@@ -16,14 +16,8 @@
 
 package org.drools.modelcompiler.builder.generator;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.github.javaparser.ast.Modifier;
@@ -278,10 +272,12 @@ public class ModelGenerator {
      */
     private static List<MethodCallExpr> ruleAttributes(RuleContext context, RuleDescr ruleDescr) {
         final List<MethodCallExpr> ruleAttributes = new ArrayList<>();
-        final Set<Entry<String, AttributeDescr>> excludingDialect =
-                ruleDescr.getAttributes().entrySet()
-                        .stream().filter(r -> !r.getKey().equals("dialect"))
-                        .collect(Collectors.toSet());
+        final Set<Entry<String, AttributeDescr>> excludingDialect = ruleDescr.getAttributes().entrySet()
+                .stream().filter(r -> !r.getKey().equals("dialect"))
+                    .sorted(Entry.comparingByKey(Comparator.reverseOrder())) // sort while streaming
+//                        .sorted(Entry.comparingByKey()) // sort while streaming
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
         for (Entry<String, AttributeDescr> as : excludingDialect) {
             MethodCallExpr attributeCall = new MethodCallExpr(null, ATTRIBUTE_CALL);
             attributeCall.addArgument(attributesMap.get(as.getKey()));
